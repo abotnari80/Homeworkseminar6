593b25100b1198a0cca2d7cfb5795a9ef6e5192e
abotnari80 <abotnari80@gmail.com>
2022-04-02 14:13:34 +0300

    Задача 1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите количество элементов массива");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+int result = 0;
+System.Console.WriteLine("Заполните массив:");
+
+for (int i = 0; i < M; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i]>0) result = result + 1; 
+}
+
+System.Console.WriteLine($"Ответ: {result}");
