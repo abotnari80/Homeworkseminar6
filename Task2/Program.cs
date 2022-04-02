79a8b9a899d0d1da7128c0a76d2572d86955fd97
abotnari80 <abotnari80@gmail.com>
2022-04-02 14:13:57 +0300

    Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите количество строк и столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[ , ] matrix = new int[m,n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i == 0 || i == m-1) matrix[i,j] = 1;
+        else if (j == 0 || j == n-1) matrix[i,j] = 1;
+        else matrix[i,j] = 0;
+        System.Console.Write(matrix[i,j] + " ");
+    }
+    System.Console.WriteLine();
+}
